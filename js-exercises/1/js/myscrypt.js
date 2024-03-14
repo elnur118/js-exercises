99bb6e494fe1369ce4f44763382b894b21983dce
elnur118 <120180082+elnur118@users.noreply.github.com>
2024-03-14 20:58:48 +0400

    Delete js-exercises directory

--- js-exercises/1/js/myscrypt.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const x = 'Həsənqulu yaxşı oğlandır';
-document.getElementById("hesen").innerHTML = x;
-
-function myFunction() {
-  document.getElementById("tapdiq").innerHTML = "Tapdıq yaxşı oğlandır";
-}
-
-
